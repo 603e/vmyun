769e35129e836194d7256182c4235af4437a7d69
liulingxian <603e@sina.com>
2018-09-24 09:36:30 +0800

    把字段code改为number

--- vmclient/src/main/java/net/vmyun/client/entity/Goods.java
+++ vmclient/src/main/java/net/vmyun/client/entity/Goods.java
@@ -22,7 +22,7 @@ public class Goods extends DataEntity<Goods> {
 	/**
 	 * 商品编码
 	 */
-	private String code;
+	private String number;
     /**
      * 商品名称
      */
@@ -44,15 +44,15 @@ public class Goods extends DataEntity<Goods> {
 		this.name = name;
 	}
 
-	public String getCode() {
-		return code;
-	}
+    public String getNumber() {
+        return number;
+    }
 
-	public void setCode(String code) {
-		this.code = code;
-	}
+    public void setNumber(String number) {
+        this.number = number;
+    }
 
-	public String getIcon() {
+    public String getIcon() {
 		return icon;
 	}
 
