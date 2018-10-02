3043af05073b53d81b3662bf2d226c190fae5953
wanghaiT <1083117451@qq.com>
2018-10-02 14:19:28 +0800

    返回信息类

---
+++ framework/src/main/java/net/vmyun/util/ResultMsg.java
@@ -0,0 +1,10 @@
+package net.vmyun.util;
+
+public class ResultMsg {
+    public String retCode;
+    public String message;
+    public ResultMsg(String retCode, String message) {
+        this.retCode = retCode;
+        this.message = message;
+    }
+}
